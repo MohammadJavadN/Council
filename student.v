312ab881dbc5167c1147234f7a00db64398647a5
MohammadJavadN <norozim17@gmail.com>
2023-05-20 17:27:09 +0430

    commit for push to rep.

---
+++ student.v
@@ -0,0 +1,35 @@
+Student{
+firstName='firstName',
+lastName='lastName',
+phoneNumber='phoneNumber',
+year='year.get()',
+examinations=[
+examination{dskj='dsc'},examination{dskj='dsc'},examination{dskj='dsc'},examination{dskj='dsc'}
+]
+
+}
+
+
+
+
+
+{	,	,	,	&	*	+	+	#	#	*	-	&	-	&	-	&	,	&	-	&	-	&	-	&	}
+
+(,) --> (-) --> (&) --> (*) --> (+) --> (#)
+
+
+        int d = convertStrToIntDate(str);
+        long l = (date.getTime() + 3600000) / 86400000;
+        x = d + l;
+        return x;
+
+
+l = (date + 3600000)/86400000
+x = convertStrToIntDate(str) +  (date + 3600000)/86400000
+
+(x - convertStrToIntDate(str))*86400000 - 3600000 = date
+
+
+
+
+
